b345a8f2948200c8889c2b29f50060cf904743f7
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-29 19:07:22 +0800

    - update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,3 +1,5 @@
+@echo off
+@REM cygwin-portable\cygwin_root\bin\x86_64-w64-mingw32-gcc.exe
 set DIR=%USERPROFILE%\Downloads\cygwin-portable
 setup-x86_64.exe "--root" "%DIR%\cygwin_root" "--quiet-mode" "noinput" "--no-shortcuts" "--no-startmenu" "--no-desktop" "--no-admin" "--no-version-check" "--no-write-registry" "--packages" "mingw64-x86_64-gcc-core" "--upgrade-also" "--only-site" "--site" "https://cygwin.mirror.constant.com/" "--local-package-dir" "%DIR%\cache"
 pause
